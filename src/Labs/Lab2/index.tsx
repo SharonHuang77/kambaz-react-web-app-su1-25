641b8a005c232d07a7fd36725afab00928a4a318
SharonHuang77 <s89huang@uwaterloo.ca>
2025-05-17 06:41:40 +0900

    lab2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,8 +1,38 @@
+import "./index.css"
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from './Margins';
+import Corners from './Corners';
+import Dimensions from './Dimensions';
+import Positions from './Positions';
+import Zindex from './Zindex';
+import Float from "./Float";
+
 export default function Lab2() {
     return (
-        <div id="wd-lab2">
-        <h2>Lab 2</h2>
-        <p>Welcome to Lab 2!</p>
-        </div>
-    );
+      <div id="wd-lab2">
+        <h2>Lab 2 - Cascading Style Sheets</h2>
+        <h3>Styling with the STYLE attribute</h3>
+        <p>
+          Style attribute allows configuring look and feel
+          right on the element. Although it's very convenient
+          it is considered bad practice and you should avoid
+          using the style attribute
+        </p>
+            
+        <ForegroundColors />
+        <BackgroundColors />
+        <Borders />
+        <Padding />
+        <Margins />
+        <Corners />
+        <Dimensions />
+        <Positions />
+        <Zindex />
+        <Float />
+      </div>
+  );
 }
+  
