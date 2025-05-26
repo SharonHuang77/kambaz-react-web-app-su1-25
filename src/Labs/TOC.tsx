57c60809a1b64bcf1667132d26c265b59c812606
SharonHuang77 <s89huang@uwaterloo.ca>
2025-05-26 09:10:31 -0700

    lab 3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,29 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// export default function TOC() {
-//     return (
-//         <ul>
-//             <li>
-//                 <Link to="/Labs">Labs</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Labs/Lab1">Lab 1</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Labs/Lab2">Lab 2</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Labs/Lab3">Lab 3</Link>
-//             </li>
-//             <li>
-//                 <Link to="/Kambaz">Kambaz</Link>
-//             </li>
-//             <li>
-//                 <Link to="https://github.com/SharonHuang77/kambaz-react-web-app-su1-25.git" id="wd-github">GitHub</Link>
-//             </li>
-//         </ul>
-//     );
-// }
 
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
