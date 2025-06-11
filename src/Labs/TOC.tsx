71dbca9d5c46bb304e2e6acf7cd74a971ead1e64
SharonHuang <s89huang@uwaterloo.ca>
2025-06-10 19:12:21 -0700

    add links

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -33,8 +33,15 @@ export default function TOC() {
                 <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link to="https://github.com/SharonHuang77/kambaz-react-web-app-su1-25.git" as={Link} id="wd-github">My GitHub</Nav.Link>
+                <Nav.Link to="https://github.com/SharonHuang77/kambaz-react-web-app-su1-25.git" as={Link} id="wd-github">My GitHub For Web</Nav.Link>
             </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="https://github.com/SharonHuang77/Kanbaz-node-server-app.git" as={Link} id="wd-github">My GitHub For Server</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link to="https://kanbaz-node-server-app-p2i0.onrender.com" as={Link} id="wd-github">Render</Nav.Link>
+            </Nav.Item>
+
         </Nav>
     );
 }
